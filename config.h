8672d1622125832efd1898d7000ea8791a9e144d
gk <messagke@gmail.com>
2022-03-06 11:59:00 +1000

    update

--- config.h
+++ config.h
@@ -6,15 +6,15 @@ static int centered = 1;                   /* -c option; centers dmenu on screen
 static int min_width = 500;                /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "Ubuntu Mono:pixelsize=13.2:style=bold" };
+static const char *fonts[] = { "Ubuntu Mono:pixelsize=16.80:style=Medium" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                  fg         bg     */
-	[SchemeNorm] = { "#83878C", "#ffffff" },
-	[SchemeSel]  = { "#5B5F66", "#f2f2f2" },
+	[SchemeNorm] = { "#B1B6BD", "#ffffff" },
+	[SchemeSel]  = { "#828791", "#f2f2f2" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
-	[SchemeSelHighlight]  = { "#5B5F66", "#f2f2f2" },
-	[SchemeNormHighlight] = { "#5B5F66", "#ffffff" },
+	[SchemeSelHighlight]  = { "#828791", "#f2f2f2" },
+	[SchemeNormHighlight] = { "#828791", "#ffffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 6;
